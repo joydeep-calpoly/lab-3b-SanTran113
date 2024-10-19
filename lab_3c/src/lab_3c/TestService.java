37d8ef5c3ac3d9412968ee99fe8398c0562f5bc6
SanTran113 <stran113@calpoly.edu>
2024-10-18 17:52:57 -0700

    another test

--- lab_3c/src/lab_3c/TestService.java
+++ lab_3c/src/lab_3c/TestService.java
@@ -1,21 +1,29 @@
 package lab_3c;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class TestService {
-	
 	@Test
 	void testDatabaseUniqueId() {
-		Database databaseMock = mock(Database.class);
+		Database databaseMock = Mockito.mock(Database.class);
 		when(databaseMock.getUniqueId()).thenReturn(42);
 		
 		int uniqueId = databaseMock.getUniqueId();
 		assertEquals(uniqueId, 42);
 	}
 
+	@Test
+	void testServiceDatabaseId() {
+		Database databaseMock = Mockito.mock(Database.class);
+		when(databaseMock.getUniqueId()).thenReturn(42);
+		
+		Service service = new Service(databaseMock);
+		
+		assertEquals("Using database with id: 42", service.getDatabaseID());
+	}
+	
 }
